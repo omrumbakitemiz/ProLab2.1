eba7b28d4fb309b6d4b24c66116729185d17fcbb
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-07-21 19:59:25 +0300

    diziler dinamiklestirildi

--- main.c
+++ main.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 #include <sys/time.h>
-
-#define MAX_KAYIT_SAYISI 381
 
 struct Ogrenci{
     char isim[11];
     int numara;
 };
 
-int kayit_sayisi_bul(struct Ogrenci ogrenciler[100]);
 void bolen_kalan_hash_ekle(struct Ogrenci ogrenciler[100]);
-void bolen_kalan_hash_ara(int ogrenciler_hash[]);
+void bolen_kalan_hash_ara();
 void kare_ortasi_hash_ara();
 void kare_ortasi_hash_ekle(struct Ogrenci ogrenciler[]);
 void lineer_arama();
-int basamak_sayisi_bul(uint64_t sayi);
 float getdiff(struct timeval endtv, struct timeval starttv);
 
-int ogrenciler_hash_bolen_kalan[MAX_KAYIT_SAYISI];
 int ogrenciler_hash_kare[10000];
-
+int kayit_sayisi = 0;
 
 int main() {
 
@@ -34,23 +28,18 @@ int main() {
     fp_names = fopen("/Users/omrumbakitemiz/CLionProjects/ProLab2.1/names.txt", "r");
     fp_numbers = fopen("/Users/omrumbakitemiz/CLionProjects/ProLab2.1/numbers.txt", "r");
 
-    struct Ogrenci ogrenciler[381];
+    printf("Lüften oluşturulacak kayıt sayısını giriniz... ");
+    scanf("%d", &kayit_sayisi);
 
-    struct timeval starttv, endtv;
-    struct timezone starttz, endtz;
+    struct Ogrenci *ogrenciler = malloc(kayit_sayisi * sizeof(struct Ogrenci));
 
-    int kayit_sayisi = 0;
-
-    //printf("Lüften oluşturulacak kayıt sayısını giriniz... ");
-    //scanf("%d", &kayit_sayisi);
-
-    for (int i = 0; i < 381 ; i++) {
+    for (int i = 0; i < kayit_sayisi ; i++) {
         fscanf(fp_names, "%s", &ogrenciler[i].isim);
         fscanf(fp_numbers, "%d", &ogrenciler[i].numara);
     }
 
     bolen_kalan_hash_ekle(ogrenciler);
-    bolen_kalan_hash_ara(ogrenciler_hash_bolen_kalan);
+    bolen_kalan_hash_ara();
     kare_ortasi_hash_ekle(ogrenciler);
     kare_ortasi_hash_ara();
     lineer_arama();
@@ -61,7 +50,6 @@ int main() {
 void kare_ortasi_hash_ekle(struct Ogrenci ogrenciler[]){
 
     int ogrenciler_hash[10000];
-    int kayit_sayisi = kayit_sayisi_bul(ogrenciler);
     int64_t hash;
     int basamaklar[18];
     int deger; // kare alındıktan sonra ortadan alınan 4 rakamlı sayı
@@ -82,7 +70,7 @@ void kare_ortasi_hash_ekle(struct Ogrenci ogrenciler[]){
 
         while(ogrenciler_hash[deger] != -1) {
             deger++;
-            if(deger == MAX_KAYIT_SAYISI){
+            if(deger == kayit_sayisi){
                 deger = 0;
             }
         }
@@ -114,7 +102,7 @@ void kare_ortasi_hash_ara(){
     int aranan_deger;
     int ogrenciler[10000];
 
-    for (int i = 0; i < 381; i++) {
+    for (int i = 0; i < kayit_sayisi; i++) {
         ogrenciler[i] = -1;
     }
 
@@ -140,38 +128,37 @@ void kare_ortasi_hash_ara(){
     gettimeofday(&endtv, &endtz);
 
     if(sonuc == 0){
-        printf("Aradığınız değer bulunamadı...");
+        printf("Kare Ortası Arama Tekniği ~~ Aradığınız değer bulunamadı...\n");
     }
     else{
-        float fark = getdiff(endtv, starttv);
-        printf("Kare Ortası Arama Tekniği Hız %f\n\n", fark);
+        int fark = getdiff(endtv, starttv);
+        printf("Kare Ortası Arama Tekniği Hız %d microsaniye\n\n", fark);
     }
 }
 
 float getdiff(struct timeval endtv, struct timeval starttv) {
     float diff=0;
     diff=(endtv.tv_sec-starttv.tv_sec) * 1000000 + (endtv.tv_usec-starttv.tv_usec);
-    return diff/1000000;
+    return diff;
 }
 
 void bolen_kalan_hash_ekle(struct Ogrenci ogrenciler[]){
 
-    int ogrenciler_hash[MAX_KAYIT_SAYISI];
-    int kayit_sayisi = kayit_sayisi_bul(ogrenciler);
+    int ogrenciler_hash[kayit_sayisi];
     int hash = 0;
 
     // Tüm kayıtlar boş hale getiriliyor...
-    for (int j = 0; j < MAX_KAYIT_SAYISI ; j++) {
+    for (int j = 0; j < kayit_sayisi ; j++) {
         ogrenciler_hash[j] = -1;
     }
 
-    for(int i=0; i < 381; i++){
+    for(int i=0; i < kayit_sayisi; i++){
 
-        hash = ogrenciler[i].numara % MAX_KAYIT_SAYISI;
+        hash = ogrenciler[i].numara % kayit_sayisi;
 
         while(ogrenciler_hash[hash] != -1) {
             hash++;
-            if(hash == MAX_KAYIT_SAYISI){
+            if(hash == kayit_sayisi){
                 hash = 0;
             }
         }
@@ -181,50 +168,53 @@ void bolen_kalan_hash_ekle(struct Ogrenci ogrenciler[]){
     FILE *hash_yaz;
     hash_yaz = fopen("/Users/omrumbakitemiz/CLionProjects/ProLab2.1/hash_bolen.txt", "w+");
 
-    for (int i = 0; i < 381; i++) {
+    for (int i = 0; i < kayit_sayisi; i++) {
         if(ogrenciler_hash[i] == -1){
             fputs("-1\n", hash_yaz);
-            ogrenciler_hash_bolen_kalan[i] = -1;
         } else{
             fprintf(hash_yaz, "%d", ogrenciler_hash[i]);
             fputs("\n", hash_yaz);
-            ogrenciler_hash_bolen_kalan[i] = ogrenciler_hash[i];
         }
     }
 }
 
-void bolen_kalan_hash_ara(int ogrenciler_hash[]){
+void bolen_kalan_hash_ara(){
 
     struct timeval starttv, endtv;
     struct timezone starttz, endtz;
 
-    int aranan_deger, hash = 0, numara;
-    int ogrenciler[381];
+    int aranan_deger;
+    int ogrenciler[kayit_sayisi];
+    int sonuc = 0;
 
     printf("aramak istenen değeri girin: ");
     scanf("%d", &aranan_deger);
-
-    hash = aranan_deger % MAX_KAYIT_SAYISI;
 
     FILE *fp;
     fp = fopen("/Users/omrumbakitemiz/CLionProjects/ProLab2.1/hash_bolen.txt", "r");
 
     gettimeofday(&starttv, &starttz);
 
-    for (int i = 0; i < MAX_KAYIT_SAYISI; i++) {
+    for (int i = 0; i < kayit_sayisi; i++) {
         fscanf(fp, "%d", &ogrenciler[i]);
 
         if(ogrenciler[i] == aranan_deger){
             printf("aradığınız değer %d hash değeriyle bulundu\n", i);
+            sonuc = 1;
             break;
         }
+        sonuc = 0;
     }
 
     gettimeofday(&endtv, &endtz);
 
-    float fark = getdiff(endtv, starttv);
-
-    printf("Bölen Kalan Tekniği Hızı : %f\n\n", fark);
+    if(sonuc == 0){
+        printf("Bölen Kalan Tekniği ~~ Aradığınız değer bulunamadı...\n");
+    }
+    else{
+        int fark = getdiff(endtv, starttv);
+        printf("Bölen Kalan Tekniği Hızı : %d microsaniye\n\n", fark);
+    }
 }
 
 void lineer_arama(){
@@ -232,8 +222,9 @@ void lineer_arama(){
     struct timeval starttv, endtv;
     struct timezone starttz, endtz;
 
-    int aranan_deger, index = 0;
-    int ogrenciler[381]; //TODO: dinamikleştirme
+    int aranan_deger;
+    int ogrenciler[kayit_sayisi];
+    int sonuc = 0;
 
     FILE *fp_lineer;
     fp_lineer = fopen("/Users/omrumbakitemiz/CLionProjects/ProLab2.1/numbers.txt", "r");
@@ -243,41 +234,24 @@ void lineer_arama(){
 
     gettimeofday(&starttv, &starttz);
 
-    for (int i = 0; i < 381; i++) { // TODO: döngü değişken dinamik yapılacak
+    for (int i = 0; i < kayit_sayisi; i++) {
         fscanf(fp_lineer, "%d", &ogrenciler[i]);
 
         if(ogrenciler[i] == aranan_deger){
             printf("aradığınız değer %d. indexte bulundu\n", i);
+            sonuc = 1;
             break;
         }
+        sonuc = 0;
     }
 
     gettimeofday(&endtv, &endtz);
 
-    float fark = getdiff(endtv, starttv);
-    printf("Lineer Arama Tekniği Hız %f\n\n", fark);
-}
-
-int basamak_sayisi_bul(uint64_t sayi){
-
-    int basamak_sayisi = 0;
-
-    while(sayi>0)
-    {
-        basamak_sayisi++;
-        sayi/=10;
+    if(sonuc == 0){
+        printf("Lineer Arama Tekniği ~~ Aradığınız değer bulunamadı...\n");
     }
-
-    return basamak_sayisi;
-}
-
-int kayit_sayisi_bul(struct Ogrenci ogrenciler[]){
-
-    int kayit_sayisi=0;
-
-    while(ogrenciler[kayit_sayisi].isim[0] != '\0'){
-        kayit_sayisi++;
+    else{
+        int fark = getdiff(endtv, starttv);
+        printf("Lineer Arama Tekniği Hız %d microsaniye\n\n", fark);
     }
-
-    return kayit_sayisi;
 }
